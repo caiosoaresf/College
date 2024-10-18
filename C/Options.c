715f8772d26b5c6a0c5327e77456fc18cf47a2de
caiosoaresf <caiosoaresf2005@gmail.com>
2024-10-17 21:57:41 -0300

    Uploaded Options.c

---
+++ C/Options.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+int n1;
+int n2;
+int n3;
+float media;
+
+printf("Insert the value of the three numbers: ");
+scanf("%d", &n1);
+scanf("%d", &n2);
+scanf("%d", &n3);
+
+int op;
+printf("choose one of the options: 1, 2 ou 3: ");
+scanf("%d", &op);
+
+switch(op){
+
+case 1:
+if((n1 > n2)&&(n1 > n3)){
+printf("The biggest number is: %d \n", n1);
+}
+else if ((n2 > n1)&&(n2 > n3)){
+printf("The biggest number is: %d \n", n2);
+}
+else if ((n3 > n1)&&(n3 > n2)){
+printf("The biggest number is: %d \n", n3);
+}
+else if ((n1 == n2)&&(n2 > n3)){
+printf("The biggest numbers are %d and %d \n", n1, n2);
+}
+else if ((n1 == n3)&&(n3 > n2)){
+printf("The biggest numbers are %d and %d \n", n1, n3);
+}
+else if ((n2 == n3)&&(n2 > n1)){
+printf("The biggest numbers are %d and %d \n", n2, n3);
+}
+else {
+printf("All number are equal and: %d \n", n1);
+}
+break;
+
+case 2:
+if((n1 < n2)&&(n1 < n3)){
+printf("The smallest number is: %d \n", n1);
+}
+else if ((n2 < n1)&&(n2 < n3)){
+printf("The smallest number is: %d \n", n2);
+}
+else if ((n3 < n1)&&(n3 < n2)){
+printf("The smallest number is: %d \n", n3);
+}
+else if ((n1 == n2)&&(n2 < n3)){
+printf("The smallest numbers are %d and %d \n", n1, n2);
+}
+else if ((n1 == n3)&&(n3 < n2)){
+printf("The smallest numbers are %d and %d \n", n1, n3);
+}
+else if ((n2 == n3)&&(n2 < n1)){
+printf("The smallest numbers are %d and %d \n", n2, n3);
+}
+else {
+printf("All number are equal and: %d \n", n1);
+}
+break;
+
+case 3:
+media = (n1 + n2 + n3)/3.0;
+printf("The arithmetic mean is: %f",media);
+break;
+
+default:
+printf("Invalid option");
+}
+}
