c2cbc7a946517e9c229c1e1e545c04deddb067f5
Caio Soares Fernandes <caiosoaresf2005@gmail.com>
2025-04-30 14:23:10 -0300

    Update Q5.c

--- AED1 PUC/Terceira Lista Avaliativa (Lista 9)/Q5.c
+++ AED1 PUC/Terceira Lista Avaliativa (Lista 9)/Q5.c
@@ -2,30 +2,38 @@
 #include <stdlib.h>
 #include <math.h>
 
-void FillValues(int arr4y[10]){
+int FillValues(int arr4y[10]) {
+    int count = 0;
     int num;
-    for(int i = 0; i < 10; i++){
+    for (int i = 0; i < 10; i++) {
         printf("Insert the value for the array: ");
         scanf("%d", &num);
         arr4y[i] = num;
-    }
-}
-
-void CopyNegatives(int arr4y[10]){
-    int arrayN[10];
-    for(int i = 0; i != "\0"; i++){
-        if (arr4y[i] < 0){
-            arrayN[i] = arr4y[i];
-            printf("%d", arrayN[i]);
-            fflush(stdout);
+        if (num < 0) {
+            count += 1;
         }
     }
+    return count;
 }
 
-int main()
-{
+void CopyNegatives(int arr4y[], int count) {
+    int arrayN[count + 1];
+    int j = 0;
+    for (int i = 0; i < 10; i++) {  
+        if (arr4y[i] < 0) {
+            arrayN[j] = arr4y[i];
+            j++;
+        } 
+    }
+    arrayN[j] = 0;
+    for (int i = 0; i < j + 1; i++) {
+        printf("%d", arrayN[i]);
+    }
+}
+
+int main() {
     int arrayX[10];
-    FillValues(arrayX);
-    CopyNegatives(arrayX);
+    int result = FillValues(arrayX);
+    CopyNegatives(arrayX, result);
     return 0;
 }
