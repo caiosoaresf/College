5b0db6eadf9e63ea05f55599de84343f36eeb4e4
Caio Soares Fernandes <caiosoaresf2005@gmail.com>
2025-05-28 18:49:00 -0300

    Create Q1.c

---
+++ AED1 PUC/Lista Revisao Prova 2/Q1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int v1[20];
+    srand(time(NULL));
+
+    for (int i = 0; i < 20; i++) {
+        int n;
+        do {
+            n = rand() % 100 + 1;
+            int rep = 0;
+            for (int j = 0; j < i; j++) {
+                if (v1[j] == n) {
+                    rep = 1;
+                    break;
+                }
+            }
+            if (n != rep) {
+                v1[i] = n;
+                break;
+            }
+        } while (1);
+    }
+
+    for (int i = 0; i < 20; i++) {
+        printf("%d ", v1[i]);
+    }
+    return 0;
+}
