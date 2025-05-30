37409e6fa8dc3102a0d45282adf45d9d70be92af
Caio Soares Fernandes <caiosoaresf2005@gmail.com>
2025-05-30 01:18:59 -0300

    Create Q10.c

---
+++ AED1 PUC/Lista Revisao Prova 2/Q10.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int inverting(int *arr, int s1ze){
+    int *start = arr;
+    int *end = arr + s1ze - 1;
+    
+    while (start < end) {
+        int temp = *start;
+        *start = *end;
+        *end = temp;
+        
+        start++;
+        end--;
+    }
+    return(1);
+}
+
+void main() {
+    int size;
+   
+    printf("Insert the size of the array: ");
+    scanf("%d", &size);
+    
+    int* arr4y = (int*)malloc(size*sizeof(int));
+    if (arr4y == NULL){
+        printf("Alloc error");
+        exit(1);
+    }
+    
+    for (int i = 0; i < size; i++){
+        scanf("%d", &arr4y[i]);
+    }
+    
+    inverting(arr4y,size);
+    for (int i = 0; i < size; i++){
+        printf("%d", arr4y[i]);
+    }
+    free(arr4y);
+}
